a8c8c4feafa0937c72107b045005adde313b57a1
miguelesco <miuelangel1999@gmail.com>
2021-09-06 17:13:25 -0400

    navbar

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import {
   Route,
 } from 'react-router-dom';
 import GlobalStyles from '../styles/GlobalStyles';
+import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
+      <Navbar />
       <Switch>
         <Route path="/" exact>
           <h1>home</h1>
