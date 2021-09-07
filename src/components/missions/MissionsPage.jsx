5364cd4b41b41f17d8f81adc1d622c3869e0daa6
carloso0114 <carlossospinag@gmail.com>
2021-09-07 12:36:08 -0500

    update styles

--- src/components/missions/MissionsPage.jsx
+++ src/components/missions/MissionsPage.jsx
@@ -7,30 +7,30 @@ const profilePage = () => (
     <table className="table caption-top table-bordered table-striped">
       <thead>
         <tr>
-          <th className="col-1">Mission</th>
-          <th className="col-9">Description</th>
-          <th className="col-1">Status</th>
+          <th className="col-1"><strong>Mission</strong></th>
+          <th className="col-9"><strong>Description</strong></th>
+          <th className="col-1"><strong>Status</strong></th>
           <th className="col-1"> </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <th>Thaicom</th>
+          <th><strong>Thaicom</strong></th>
           <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-          <td className="text-center"><span className="not-member">NOT A MEMBER</span></td>
-          <td><button type="button" className="btn btn-outline-secondary">Join Mission</button></td>
+          <td className="align-middle"><span className="not-member">NOT A MEMBER</span></td>
+          <td className="text-center align-middle"><button type="button" className="btn btn-outline-secondary">Join Mission</button></td>
         </tr>
         <tr>
-          <th>Telstar</th>
+          <th><strong>Telstar</strong></th>
           <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-          <td className="text-center"><span className="active-member">Active Member</span></td>
-          <td><button type="button" className="btn btn-outline-danger">Leave Mission</button></td>
+          <td className="align-middle"><span className="active-member">Active Member</span></td>
+          <td className="text-center align-middle"><button type="button" className="btn btn-outline-danger">Leave Mission</button></td>
         </tr>
         <tr>
-          <th>Iridium Next</th>
+          <th><strong>Iridium Next</strong></th>
           <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-          <td className="text-center"><span className="not-member">NOT A MEMBER</span></td>
-          <td><button type="button" className="btn btn-outline-secondary">Join Mission</button></td>
+          <td className="align-middle"><span className="not-member">NOT A MEMBER</span></td>
+          <td className="text-center align-middle"><button type="button" className="btn btn-outline-secondary">Join Mission</button></td>
         </tr>
       </tbody>
     </table>
