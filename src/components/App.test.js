a5279a846ae03dbfc9f4a22c150a901cd4ab11b2
miguelesco <miuelangel1999@gmail.com>
2021-09-09 13:26:24 -0400

    update test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from '../redux/configureStore';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(app).toMatchSnapshot();
 });
